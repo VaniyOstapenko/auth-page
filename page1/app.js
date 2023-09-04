5449a4d619a4b5818dcc3cc324fc038e55f138ce
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-09-05 00:06:26 +0300

    added JS

--- page1/app.js
+++ page1/app.js
@@ -1,19 +1,26 @@
-const btn = document.querySelector('.button');
+class AuthPage {
+    doPage1() {
+        const btn = document.querySelector('.button');
 
-btn.addEventListener('click', function () {
-    try {
-        const inp = document.querySelector('.input1');
-        const inp2 = document.querySelector('.input2')
-        if (!/^[\w\.\-\$]+@[a-z]+\.[a-z]{2,4}|\+[0-9]{12}$/g.test(inp.value)) {
-            throw new Error(`ошибка ввода e-mail or phone number`)
-        } if (!/^[\w\W]{8,100}$/g.test(inp2.value)) {
-            throw new Error(`ошибка ввода password`)
-        }
-        alert('авторизованы в системе');
-        inp.value = '';
-        inp2.value = '';
-    } catch (error) {
-        alert(error.message);
+        btn.addEventListener('click', function () {
+            try {
+                const inp = document.querySelector('.input1');
+                const inp2 = document.querySelector('.input2')
+                if (!/^[\w\.\-\$]+@[a-z]+\.[a-z]{2,4}|\+[0-9]{12}$/g.test(inp.value)) {
+                    throw new Error(`ошибка ввода e-mail or phone number`)
+                } if (!/^[\w\W]{8,100}$/g.test(inp2.value)) {
+                    throw new Error(`ошибка ввода password`)
+                }
+                alert('авторизованы в системе');
+                inp.value = '';
+                inp2.value = '';
+            } catch (error) {
+                alert(error.message);
+            }
+        })
     }
+}
 
-})
+const authPage = new AuthPage();
+const result = authPage.doPage1()
+console.log(result);
--- page2/app2.js
+++ page2/app2.js
@@ -1,23 +1,30 @@
-const btn = document.querySelector('.button');
+class AuthPage {
+    doPage2() {
+        const btn = document.querySelector('.button');
 
-btn.addEventListener('click', function () {
-    try {
-        const inp1 = document.querySelector('.input1');
-        const inp2 = document.querySelector('.input2');
-        const inp3 = document.querySelector('.input3');
-        if (!/^[\w\.\-\$]+@[a-z]+\.[a-z]{2,4}|\+[0-9]{12}$/g.test(inp1.value)) {
-            throw new Error('ошибка ввода e-mail or phone number')
-        } if (!/^[\w\W]{8,100}$/g.test(inp2.value) || !/^[\w\W]{8,100}$/g.test(inp3.value)) {
-            throw new Error(`ошибка ввода password`)
-        } if (inp2.value !== inp3.value) {
-            throw new Error('Ошибка ввода (не одинаковые пароли)')
-        }
-        alert('Вы успешно зарегистрированы')
-        inp1.value = '';
-        inp2.value = '';
-        inp3.value = '';
-    } catch (error) {
-        alert(error.message);
+        btn.addEventListener('click', function () {
+            try {
+                const inp1 = document.querySelector('.input1');
+                const inp2 = document.querySelector('.input2');
+                const inp3 = document.querySelector('.input3');
+                if (!/^[\w\.\-\$]+@[a-z]+\.[a-z]{2,4}|\+[0-9]{12}$/g.test(inp1.value)) {
+                    throw new Error('ошибка ввода e-mail or phone number')
+                } if (!/^[\w\W]{8,100}$/g.test(inp2.value) || !/^[\w\W]{8,100}$/g.test(inp3.value)) {
+                    throw new Error(`ошибка ввода password`)
+                } if (inp2.value !== inp3.value) {
+                    throw new Error('Ошибка ввода (не одинаковые пароли)')
+                }
+                alert('Вы успешно зарегистрированы')
+                inp1.value = '';
+                inp2.value = '';
+                inp3.value = '';
+            } catch (error) {
+                alert(error.message);
+            }
+        })
     }
+}
 
-})
+const authPage = new AuthPage();
+const result = authPage.doPage2();
+console.log(result);
